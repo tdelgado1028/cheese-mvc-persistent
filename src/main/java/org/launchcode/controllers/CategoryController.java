400b96724778640801b1cefb7b7a41c48ab8aa8e
Tania Delgado <tdelgado1028@gmail.com>
2017-04-25 11:46:33 -0500

    Part1- Adding Categories -- CategoryController class

---
+++ src/main/java/org/launchcode/controllers/CategoryController.java
@@ -0,0 +1,18 @@
+package org.launchcode.controllers;
+
+import org.launchcode.models.data.CategoryDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Rhodondron on 4/25/2017.
+ */
+@Controller
+@RequestMapping("category")
+public class CategoryController {
+
+    @Autowired
+    private CategoryDao categoryDao;
+
+}
