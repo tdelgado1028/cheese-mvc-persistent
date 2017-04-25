1336c965d98ec5e9a20eff01e123985f2e356e94
Tania Delgado <tdelgado1028@gmail.com>
2017-04-25 11:35:18 -0500

    Part1- Setting Up A New Model -- Category class

---
+++ src/main/java/org/launchcode/models/Category.java
@@ -0,0 +1,42 @@
+package org.launchcode.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ * Created by Rhodondron on 4/25/2017.
+ */
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @NotNull
+    @Size(min=3, max=15)
+    private String name;
+
+    public Category(){}
+
+    public Category(String name){
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
