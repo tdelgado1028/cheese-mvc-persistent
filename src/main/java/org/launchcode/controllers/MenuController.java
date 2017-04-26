cb49bcad9e632640c1fe478c72d9284bb3867f94
Tania Delgado <tdelgado1028@gmail.com>
2017-04-26 13:17:40 -0500

    Part3- *Display/Process Add Menu (Form)* -- currently setting up MenuController.java to handle adding new Menus, took liberty of copy/paste modelling after the CheeseController.java methods so have divied up tasks as prepping MenuController.java first...

--- src/main/java/org/launchcode/controllers/MenuController.java
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -1,10 +1,17 @@
 package org.launchcode.controllers;
 
-import org.launchcode.models.data.CategoryDao;
+import org.launchcode.models.Menu;
+import org.launchcode.models.data.MenuDao;
 import org.launchcode.models.data.CheeseDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 /**
  * Created by Rhodondron on 4/26/2017.
@@ -18,8 +25,36 @@ public class MenuController {
     private CheeseDao cheeseDao;
 
     @Autowired
-    private CategoryDao categoryDao;
+    private MenuDao menuDao;
 
-    
+    @RequestMapping(value = "")
+    public String index(Model model){
 
+        model.addAttribute("title", "Menu");
+        model.addAttribute("menus", menuDao.findAll());
+
+        return "menu/index";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayAddMenu(Model model) {
+        model.addAttribute("title", "Add New Menu");
+        model.addAttribute(new Menu());
+        return "menu/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAddMenuForm(@Valid @ModelAttribute Menu newMenu, Errors errors, Model model) {
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add New Menu");
+            model.addAttribute(new Menu());
+            model.addAttribute(errors); //*??
+            return "menu/add";
+        }
+
+        menuDao.save(newMenu);
+
+        return "redirect:view/" + newMenu.getId();
+    }
 }
