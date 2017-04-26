7ea95df898d983118aceaaaf45a8cea17cf20ab1
Tania Delgado <tdelgado1028@gmail.com>
2017-04-26 09:02:58 -0500

    Part3- The Menu Class -- created class Menu.java with name, id, and cheeses fields and a addItem method

---
+++ src/main/java/org/launchcode/models/Menu.java
@@ -0,0 +1,50 @@
+package org.launchcode.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+/**
+ * Created by Rhodondron on 4/26/2017.
+ */
+
+@Entity
+public class Menu {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @NotNull
+    @Size(min = 3, max = 15)
+    private String name;
+
+    @ManyToMany
+    private List<Cheese> cheeses;
+
+    public Menu(String name) {
+        this.name = name;
+    }
+
+    public Menu(){}
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void addItem(Cheese item){
+        cheeses.add(item);
+    }
+}
